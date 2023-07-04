2d0840938ac882fc20b276e6faf74032a27a3667
AleLeone09 <ale.leone09@gmail.com>
2023-07-04 17:12:59 -0300

    Refactor nombre carpetas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import WodRandomizerApp from './components/todo/WodRandomizerApp';
+import WodRandomizerApp from './components/randomizer/WodRandomizerApp';
 
 
 function App() {
