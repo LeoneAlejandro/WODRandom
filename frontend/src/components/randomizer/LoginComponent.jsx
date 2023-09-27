ddfd6c4b7f88558436a8278f0240d3f07540f338
AleLeone <ale.leone09@gmail.com>
2023-09-27 18:48:41 -0300

    Random Legacy Wods added

--- frontend/src/components/randomizer/LoginComponent.jsx
+++ frontend/src/components/randomizer/LoginComponent.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from './security/AuthContext'
+import wodsData from '../../assets/legacyWods.json'
+import '../css/LoginComponent.css'
 
 export default function LoginComponent() {
 
@@ -28,29 +30,64 @@ export default function LoginComponent() {
         }
     }
 
+    //Legacy Wods
+
+    const [randomWOD, setRandomWOD] = useState(null);
+
+    const getRandomWOD = () => {
+      const randomIndex = Math.floor(Math.random() * wodsData.WODs.length);
+      const selectedWOD = wodsData.WODs[randomIndex];
+      setRandomWOD(selectedWOD);
+    }
+
 
     return(
-        <div className="Login">
-            <br />
-            <h1>Iniciá sesión</h1>
-            {showErrorMessage && <div className="errorMessage">Usuario o contraseña incorrectos</div>}
-            
-            <div className="loginForm">
-                <div className="inputbox">
-                    <input required="required" type="text" value={username} onChange={handleUsernameChange}/>
-                    <span>Usuario</span>
-                    <i></i>
+        
+        <div className='loginMenu'>
+
+            <div className="login">
+                <div className='loginCard'>
+                    <h1>Iniciá sesión</h1>
+                    {showErrorMessage && <div className="errorMessage">Usuario o contraseña incorrectos</div>}
+                    
+                    <div className="loginForm">
+                        <div className="inputbox">
+                            <input required="required" type="text" value={username} onChange={handleUsernameChange}/>
+                            <span>Usuario</span>
+                            <i></i>
+                        </div>
+                        <div className="inputbox">
+                            <input required="required" type="password" value={password} onChange={handlePasswordChange}/>
+                            <span>Contraseña</span>
+                            <i></i>
+                        </div>
+                        <br />
+                        <div>
+                            <button type="button" name="login " onClick={handleSubmit}> 
+                                Ingresar </button>
+                        </div>
+                    </div>
+
                 </div>
-                <div className="inputbox">
-                    <input required="required" type="password" value={password} onChange={handlePasswordChange}/>
-                    <span>Contraseña</span>
-                    <i></i>
-                </div>
-                <br />
-                <div>
-                    <button type="button" name="login " onClick={handleSubmit}> 
-                        Ingresar </button>
-                </div>
+            </div>
+
+            <div className="legacyWods">
+                    <h1>WODs Tradicionales</h1>
+                    <button className='legacyWodsButton' onClick={getRandomWOD}>Random WOD</button>
+                    {randomWOD && (
+                        <div className='legacyWodsList'>
+                            {/* <p>ID: {randomWOD.id}</p> */}
+                            <p className='legacyWodsListName'>{randomWOD.name}</p>
+                            {/* <p>Exercises:</p> */}
+                            <tr >
+                                {randomWOD.exercises.map((exercise, index) => (
+                                <tr className='legacyWodSingleRow' key={index}>{exercise}</tr>
+                                ))}
+                            </tr>
+                            {randomWOD.description && <p className='lWLDescription'>{randomWOD.description} </p>
+                            }
+                        </div>
+                    )}
             </div>
         </div>
     )
--- frontend/src/components/randomizer/WelcomeComponent.jsx
+++ frontend/src/components/randomizer/WelcomeComponent.jsx
@@ -1,40 +1,89 @@
 import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
 import '../css/WelcomeComponent.css'
+import wodsData from '../../assets/legacyWods.json'
 
 export default function WelcomeComponent() {
 
     const navigate = useNavigate()
 
+    const [randomWOD, setRandomWOD] = useState(null);
+
+    const getRandomWOD = () => {
+      const randomIndex = Math.floor(Math.random() * wodsData.WODs.length);
+      const selectedWOD = wodsData.WODs[randomIndex];
+      setRandomWOD(selectedWOD);
+    };
+
     return(
+        <div className='welcomeMenu'>
+
             <div className='containerCards'>
+
                 <div className="menuCard">
                     <div className="menuCard-content" onClick={() => navigate(`/randomize`)}>
-                        <h3 className="menuCard-title">Randomizar</h3>
+                        <h3 className="menuCard-title">Randomizar !</h3>
                         <h4 className="menuCard-subtitle">creá un wod aleatorio seleccionado de tus ejercicios favoritos</h4>
                     </div>
                 </div>
 
                 <div className="menuCard">
                     <div className="menuCard-content" onClick={() => navigate(`/wods`)}>
-                        <h3 className="menuCard-title" >Wods</h3>
+                        <h3 className="menuCard-title" >WODs</h3>
                         <h4 className="menuCard-subtitle">accedé a todos tus wods favoritos guardados, también podés editar wods existentes</h4>
                     </div>
                 </div>
 
                 <div className="menuCard">
                     <div className="menuCard-content" onClick={() => navigate(`/exercises`)} >
-                        <h3 className="menuCard-title" >Ejercicios</h3>
+                        <h3 className="menuCard-title" >Tus ejercicios</h3>
                         <h4 className="menuCard-subtitle">agregá tus ejercicios preferidos para crear wods aleatorios o customizados a tu elección</h4>
                     </div>
                 </div>
 
                 <div className="menuCard">
                     <div className="menuCard-content"  onClick={() => navigate(`/createWod/-1`)}>
-                        <h3 className="menuCard-title">Custom Wods</h3>
-                        <h4 className="menuCard-subtitle">creá Wods a tu medida, eligiendo a tu elección tus ejercicios preferidos</h4>
+                        <h3 className="menuCard-title">Custom WODs</h3>
+                        <h4 className="menuCard-subtitle">creá Wods a tu medida, eligiendo a elección tus ejercicios preferidos</h4>
                     </div>
+                </div>
+
+            </div>
+
+            {/* <div className="legacyWods">
+                <h2>Legacy Wods</h2>
+                <button onClick={getRandomWOD}>Pick a Random WOD</button>
+                {randomWOD && (
+                    <div>
+                        <p>Name: {randomWOD.name}</p>
+                        <tr className='legacyWodsList'>
+                            {randomWOD.exercises.map((exercise, index) => (
+                            <tr key={index}>{exercise}</tr>
+                            ))}
+                        </tr>
+                    </div>
+                )}
+            </div> */}
+
+            <div className="legacyWodsContainer">
+                <div className='legacyWodCard'>
+                    <h2 className='legacyWodTitle'>Legacy Wods</h2>
+                    <button className='legacyWodsWelcomeButton' onClick={getRandomWOD}>Random WOD</button>
+                    {randomWOD && (
+                        <div className='legacyWodsList'>
+                            <p className='legacyWodsListTitle'>{randomWOD.name}</p>
+                            <tr className='legacyWodRow'>
+                                {randomWOD.exercises.map((exercise, index) => (
+                                <tr className='legacyWodSingleRow' key={index}>{exercise}</tr>
+                                ))}
+                            </tr>
+                                {randomWOD.description && <p className='lWWLDescription'>{randomWOD.description} </p>
+                            }
+                        </div>
+                    )}
                 </div>
             </div>
 
+        </div>
         )
 }
