b2d3c0e07dd4217c2eb119c24e5941ace8363f03
Alejandro Leone <88694388+LeoneAlejandro@users.noreply.github.com>
2023-11-02 17:31:48 -0300

    Update EmailConfig.java

--- backend/src/main/java/com/aleleone/WOD/Randomizer/configuration/EmailConfig.java
+++ backend/src/main/java/com/aleleone/WOD/Randomizer/configuration/EmailConfig.java
@@ -10,11 +10,7 @@ public class EmailConfig {
 
 	@Bean
     public JavaMailSender javaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("c1930970.ferozo.com"); // Set your SMTP host
-        mailSender.setPort(465); // Set the port for SMTP (e.g., 587 for TLS)
-        mailSender.setUsername("alejandroleone@terramas.com.ar"); // Set your email username
-        mailSender.setPassword("CorreosNuevos2020"); // Set your email password
+
 
         return mailSender;
     }
